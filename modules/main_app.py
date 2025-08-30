744ec257d207a95a00a791fba1633a623f06f078
robinwojcik3 <robin.wojcik3@gmail.com>
2025-08-30 16:33:32 +0200

    Update main_app.py

--- modules/main_app.py
+++ modules/main_app.py
@@ -285,6 +285,7 @@ PY_VER    = "Python312"
 # --- Global Paths & Constants ---
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..')) 
+PREFS_PATH = os.path.join(BASE_DIR, "prefs.json")
 
 
 
